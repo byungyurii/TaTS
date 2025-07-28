2a80b2229445bcdfa909f65d87a66cc66c2e05c8
syrhee <109809295+sy12h@users.noreply.github.com>
2025-07-28 19:38:16 +0900

    Update main_forecast_multimodal_ours_2.sh

--- scripts/main_forecast_multimodal_ours_2.sh
+++ scripts/main_forecast_multimodal_ours_2.sh
@@ -1,21 +1,21 @@
-#all_models=("iTransformer" "Crossformer" "DLinear" "FEDformer" "FiLM" "Autoformer" "Informer" "Transformer")
-all_models=("DLinear" "FiLM")
+all_models=("iTransformer" "Crossformer" "DLinear" "FEDformer" "FiLM" "Autoformer" "Informer" "Transformer")
+#all_models=("DLinear" "FiLM")
 GPU=3
 
 root_path=./data
 
 seeds=(2025)
 
-datasets=("Energy")
+datasets=("Economy")
 
 current_dir=$(pwd)
 
-prior_weight=0
+prior_weight=0.5
 prompt_weight=1     #unimodal 돌릴 때 0, multimodal 1 
 text_emb=12
 nce_weight=0.1
-pred_lengths=(12 24 36 48)
-#pred_lengths=(6 8 10 12)
+#pred_lengths=(12 24 36 48)
+pred_lengths=(6 8 10 12)
 #pred_lengths=(48 96 192 336)
 
 for seed in "${seeds[@]}"
@@ -46,14 +46,15 @@ do
                     --des Exp \
                     --seed $seed \
                     --prior_weight $prior_weight \
-                    --save_name multimodal_0728_ours_infoNCEloss_energy_gpt2_pw0_nw0.1 \
+                    --save_name multimodal_0728_ours_infoNCEloss_economy_gpt2_pw0.5_nw0.1 \
                     --prompt_weight $prompt_weight \
                     --llm_model GPT2 \
                     --huggingface_token NA \
                     --train_epochs 100 \
                     --patience 20 \
                     --features S \
-                    --use_fullmodel 0 
+                    --use_fullmodel 0 \
+                    --nce_weight ${nce_weight} 
             done
         done
     done
